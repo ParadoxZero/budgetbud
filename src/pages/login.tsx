dc800fa2533ffd79a93feb3811aa613cbd711452
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-06-16 19:39:23 +0530

    Fix formatting

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,7 @@ import { GoogleOutlined, GithubOutlined, InstagramOutlined, LinkOutlined } from 
 
 function Socials() {
     return (
-        <Flex gap = {5} align="center" justify="space-evenly" dir="row">
+        <Flex gap={5} align="center" justify="space-evenly" dir="row">
             <GoogleOutlined />
             <GithubOutlined />
             <InstagramOutlined />
@@ -16,27 +16,23 @@ function Socials() {
 
 function LoginForm() {
     return (
-        <>
-            <Card bordered={GetScreenSize() == ScreenSize.desktop}>
-                <Flex gap={10} align="center" vertical>
-                    <Input placeholder="Username" />
-                    <Input placeholder="Password" type="password" />
-                    <Button type="primary">Login</Button>
-                    <Button type="link">Forgot Password?</Button>
-                    <p> No account yet? <Button type="link" style={{ padding: 0 }}>Register now</Button></p>
-                    <Socials />
-                </Flex>
-            </Card>
-        </>
+        <Card bordered={GetScreenSize() == ScreenSize.desktop}>
+            <Flex gap={10} align="center" vertical>
+                <Input placeholder="Username" />
+                <Input placeholder="Password" type="password" />
+                <Button type="primary">Login</Button>
+                <Button type="link">Forgot Password?</Button>
+                <p> No account yet? <Button type="link" style={{ padding: 0 }}>Register now</Button></p>
+                <Socials />
+            </Flex>
+        </Card>
     )
 }
 
 export function LoginPage() {
     return (
-        <>
         <Flex align="center" justify="space-around" vertical >
             <LoginForm />
         </Flex>
-        </>
     )
 }
