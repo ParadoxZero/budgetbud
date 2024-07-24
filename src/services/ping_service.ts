cc97a8447e644b8c507409407027683614733957
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-07-24 11:28:16 +0530

    Include cookies in the ping request.

--- src/services/ping_service.ts
+++ src/services/ping_service.ts
@@ -5,7 +5,9 @@ const ping_endpoint: string = "/.auth/me";
 
 export async function PingRemote(): Promise<Object> {
     try {
-        const response = await fetch(remote_domain + ping_endpoint);
+        const response = await fetch(remote_domain + ping_endpoint, {
+            credentials: 'include'
+        });
         return JSON.parse(await response.text());
     } catch (error) {
         return Promise.reject(error);
