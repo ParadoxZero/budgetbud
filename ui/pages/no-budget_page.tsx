baf5b255ab3f9eb87d419a18548e9662441afddb
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-07-28 13:57:30 +0530

    Add alert for uneditable categories

--- ui/pages/no-budget_page.tsx
+++ ui/pages/no-budget_page.tsx
@@ -1,11 +1,14 @@
-import { Card, Flex } from 'antd';
+import { Alert, Card, Flex } from 'antd';
 import React from 'react';
 import { CreateBudgetForm } from '../components/create_budget_form';
 
 export class NoBudgetAvailablePage extends React.Component {
     render() {
         return (
-            <CreateBudgetForm />
+            <>
+                <Alert message="As of now, categories cannot be edited once added. So please configure your budget carefully." type="warning" />
+                <CreateBudgetForm />
+            </>
         );
     }
 }
