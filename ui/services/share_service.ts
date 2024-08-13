7edfc70443cf726bae39d1d261e5083531d0a1a2
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-08-14 00:20:55 +0530

    Add share service

---
+++ ui/services/share_service.ts
@@ -0,0 +1,34 @@
+/* 
+ * BudgetBug - Budgeting and Expense Tracker with WebUI and API server
+ * Copyright (C) 2024  Sidhin S Thomas <sidhin.thomas@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * The source is available at: https://github.com/ParadoxZero/budgetbud
+ */
+
+import { fetchData } from "./network_service";
+
+export interface ShareKeyResponse {
+    shareKey: string;
+}
+
+export async function GetShareKey(budget_id: string): Promise<ShareKeyResponse> {
+    if (import.meta.env.VITE_USE_LOCAL_DATA_SERVICE === 'true') {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        return { shareKey: "2U0T0Y05" };
+    }
+    const response = await fetchData("/api/Social/share/{budget_id}", {});
+    return await response.json();
+}
