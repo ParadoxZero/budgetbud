c459f56bd55f8b0779122816a4f387749c238ca6
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-08-13 12:25:01 +0530

    add login refresh flow

--- ui/login.tsx
+++ ui/login.tsx
@@ -22,11 +22,55 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './main.css'
-import { Button, Card, Flex, Typography } from 'antd'
-import { GithubOutlined, GoogleOutlined } from '@ant-design/icons'
+import { Button, Card, Flex, Spin, Typography } from 'antd'
+import { GithubOutlined, GoogleOutlined, Loading3QuartersOutlined, LoadingOutlined } from '@ant-design/icons'
 
 
-class App extends React.Component {
+interface AppState {
+    is_loading: boolean;
+}
+class App extends React.Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props)
+        this.state = {
+            is_loading: true
+        }
+    }
+
+    componentDidMount(): void {
+        fetch('/.auth/refresh').then((response) => {
+            if (response.ok) {
+                window.location.href = '/index.html'
+            }
+        }).finally(() => {
+            this.setState({ is_loading: false })
+        });
+    }
+
+    renderCallToAction() {
+        if (this.state.is_loading) {
+            return (
+                <Spin size='large' indicator={<LoadingOutlined />} />
+            )
+        } else {
+            (
+                <>
+                    <Button type='primary'
+                        style={{ backgroundColor: '#f5222d' }}
+                        href='.auth/login/google?post_login_redirect_uri=/index.html&access_type=offline'
+                        size='large' block icon={<GoogleOutlined />}>
+                        Login With Google
+                    </Button>
+                    <Button type='primary'
+                        style={{ backgroundColor: '#262626' }}
+                        href='.auth/login/github?post_login_redirect_uri=/index.html'
+                        size='large' block icon={<GithubOutlined />}>
+                        Login With Github
+                    </Button>
+                </>
+            )
+        }
+    }
     render(): React.ReactNode {
         return (
             <>
@@ -34,18 +78,7 @@ class App extends React.Component {
                     <Card bordered style={{ width: 300 }} hoverable>
                         <Flex justify='center' align='center' vertical gap={20}>
                             <Typography.Title level={1}>BudgetBud</Typography.Title>
-                            <Button type='primary'
-                                style={{ backgroundColor: '#f5222d' }}
-                                href='.auth/login/google?post_login_redirect_uri=/index.html'
-                                size='large' block icon={<GoogleOutlined />}>
-                                Login With Google
-                            </Button>
-                            <Button type='primary'
-                                style={{ backgroundColor: '#262626' }}
-                                href='.auth/login/github?post_login_redirect_uri=/index.html'
-                                size='large' block icon={<GithubOutlined />}>
-                                Login With Github
-                            </Button>
+                            {this.renderCallToAction()}
                         </Flex>
                     </Card >
                 </Flex>
